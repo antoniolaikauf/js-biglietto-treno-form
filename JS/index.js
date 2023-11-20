8d61fb993390a081ac20a58e12b2c3a02ee7bedc
antoniolaikauf <antonio.laikauf00@gmail.com>
2023-11-20 16:38:27 +0100

    aggiunto commenti

--- JS/index.js
+++ JS/index.js
@@ -6,12 +6,12 @@
 // L’output del prezzo finale va messo fuori in forma umana (con massimo due decimali, per indicare centesimi sul prezzo).
 // Questo richiederà un minimo di ricerca.
 
-// prendere valori e metterli dentro variabili 
+// prendere valori  utente e metterli dentro variabili 
 let nomeUtente = document.querySelector("#nome");
 let chilometriUtente = document.querySelector("#chilometri");
 let ageUtente = document.querySelector("#età");
 
-
+// inizio evento click sulla generazione del biglietto
 const generazione = document.querySelector("#genera-biglietto")
 generazione.addEventListener("click", function() {
     let chilometriPercorrere = parseInt(chilometriUtente.value);
@@ -30,21 +30,28 @@ generazione.addEventListener("click", function() {
     }
     prezzo = prezzo - sconto;
     // console.log(prezzo, sconto);
-    // numero carrozza
+    // numero carrozza fatto randomico
     let carrozza= Math.floor((Math.random() * 100))
-    // numero codice cp
+    // numero codice cp fatto randomico
     let numerocp=Math.floor((Math.random() * 10000))
     // elementi aggiutni dentro html al funzionamento dell'evento
-    document.getElementById("costo-biglietto").innerHTML=prezzo.toFixed(2);
+    // prezzo biglietto
+    document.getElementById("costo-biglietto").innerHTML=prezzo.toFixed(2); 
+    // nome utente
     document.getElementById("nome-passeggero").innerHTML=nomeUtente.value;
+    // fascia età
     document.getElementById("fascia-età").innerHTML=age;
+    // numero carozza
     document.getElementById("n-carrozza").innerHTML=carrozza;
+    // numerocp
     document.getElementById("codicecp").innerHTML=numerocp;
+    // aggiunta delle caratteristiche del biglietto
     document.getElementById("risultato").style.display="block";
 })
 
-
+// evento per annullare il biglietto
 const cancellazione=document.querySelector("#annulla-biglietto");
 cancellazione.addEventListener("click", function () {
+    // aggiunta delle caratteristiche del biglietto
     document.getElementById("risultato").style.display="none";
 })
